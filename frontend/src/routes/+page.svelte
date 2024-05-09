8e49694e2073a47545312c8b9e7c05fcb4589197
bfoggo <b.j.foggo@gmail.com>
2024-05-08 22:09:04 -0500

    grid item toggles

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,12 +2,12 @@
     const numKeys = 12;
     const numBeats = 16;
     const beatWidth = 20;
-    const keyHeight = 15;
+    const keyHeight = 10;
     
-    let pianoRoll = Array(numBeats).fill(Array(numKeys).fill(false));
+    let pianoRoll = Array.from({ length: numKeys }, () => Array.from({ length: numBeats }, () => false));
 
-    function toggleNote(beat: number, key: number) {
-        pianoRoll[beat][key] = !pianoRoll[beat][key];
+    function toggleNote(key: number, beat: number) {
+        pianoRoll[key][beat] = !pianoRoll[key][beat];
         pianoRoll = [...pianoRoll];
     }
 
@@ -29,7 +29,10 @@
 <div class="piano-roll" style="grid-template-columns: repeat({numBeats}, {beatWidth}px)"> 
     {#each pianoRoll as key, keyIndex}
         {#each key as beat, beatIndex}
-            <div class="cell"  style="background-color: {beat ? 'black' : 'white'}; width: {beatWidth}px; height: {keyHeight}px"></div>
+            <button type="button" class="cell" style="width: {beatWidth}px; height: {keyHeight}px; 
+            background-color: {pianoRoll[keyIndex][beatIndex] ? 'black' : 'white'}"
+                on:click={() => toggleNote(keyIndex, beatIndex)}>
+            </button>
         {/each}
     {/each}
 </div>
