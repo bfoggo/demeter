2e69461c52a4832195b14603fe0850060e3dbd66
bfoggo <b.j.foggo@gmail.com>
2024-05-08 22:31:28 -0500

    basic ugly keyboard

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    const numKeys = 12;
+    const keyNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    let numKeys = keyNames.length;
+    const keyColors = ['white', 'black', 'white', 'black', 'white', 'white', 'black', 'white', 'black', 'white', 'black', 'white']
     const numBeats = 16;
     const beatWidth = 20;
     const keyHeight = 10;
@@ -14,6 +16,11 @@
 </script>
 
 <style>
+
+    .keyboard {
+        display: grid;
+    }
+
     .piano-roll {
         display: grid;
     }
@@ -24,15 +31,28 @@
         margin: 0;
     }
 
-</style>
+    .key {
+        border: 1px solid #ccc;
+        cursor: pointer;
+        margin: 0;
+    }
 
-<div class="piano-roll" style="grid-template-columns: repeat({numBeats}, {beatWidth}px)"> 
-    {#each pianoRoll as key, keyIndex}
-        {#each key as beat, beatIndex}
-            <button type="button" class="cell" style="width: {beatWidth}px; height: {keyHeight}px; 
-            background-color: {pianoRoll[keyIndex][beatIndex] ? 'black' : 'white'}"
-                on:click={() => toggleNote(keyIndex, beatIndex)}>
-            </button>
+</style>
+<div style="display:flex; justify-content: flex-start">
+    <div class="keyboard" style="grid-template-rows: repeat({numKeys}, {keyHeight}px); margin-right: 5px;">
+        {#each Array.from({ length: numKeys }, (_, i) => i) as key}
+            <div class="key" style="height: {keyHeight}px; width: 20px; background-color: {keyColors[key]}">
+            </div>
         {/each}
-    {/each}
+    </div>
+    <div class="piano-roll" style="grid-template-columns: repeat({numBeats}, {beatWidth}px)">
+        {#each pianoRoll as key, keyIndex}
+            {#each key as beat, beatIndex}
+                <button type="button" class="cell" style="width: {beatWidth}px; height: {keyHeight}px; 
+                background-color: {pianoRoll[keyIndex][beatIndex] ? 'black' : 'white'}"
+                    on:click={() => toggleNote(keyIndex, beatIndex)}>
+                </button>
+            {/each}
+        {/each}
+    </div>
 </div>
