3a2cc3aa2b22dfe4375405d2b1d8912a67669e4a
bfoggo <b.j.foggo@gmail.com>
2024-05-08 21:55:12 -0500

    basic grid

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    const numKeys = 12;
+    const numBeats = 16;
+    const beatWidth = 20;
+    const keyHeight = 15;
+    
+    let pianoRoll = Array(numBeats).fill(Array(numKeys).fill(false));
+
+    function toggleNote(beat: number, key: number) {
+        pianoRoll[beat][key] = !pianoRoll[beat][key];
+        pianoRoll = [...pianoRoll];
+    }
+
+</script>
+
+<style>
+    .piano-roll {
+        display: grid;
+    }
+
+    .cell{
+        border: 1px solid #ccc;
+        cursor: pointer;
+        margin: 0;
+    }
+
+</style>
+
+<div class="piano-roll" style="grid-template-columns: repeat({numBeats}, {beatWidth}px)"> 
+    {#each pianoRoll as key, keyIndex}
+        {#each key as beat, beatIndex}
+            <div class="cell"  style="background-color: {beat ? 'black' : 'white'}; width: {beatWidth}px; height: {keyHeight}px"></div>
+        {/each}
+    {/each}
+</div>
