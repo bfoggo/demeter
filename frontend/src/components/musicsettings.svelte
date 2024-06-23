fae7c138d0eb649f0a2f42942590b23e6a89884f
bfoggo <b.j.foggo@gmail.com>
2024-06-23 13:11:25 -0500

    decluttered menu styling

--- frontend/src/components/musicsettings.svelte
+++ frontend/src/components/musicsettings.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-    import {TimeSignature, allDivisions, allTuplets} from "../types/pianoRoll";
+    import {
+        TimeSignature,
+        allDivisions,
+        allTuplets,
+    } from "../types/pianoRoll";
     import type { Writable } from "svelte/store";
     import Select from "flowbite-svelte/Select.svelte";
     import type { MusicContext } from "../types/context";
     import Input from "flowbite-svelte/Input.svelte";
-    export let musicContext: Writable<MusicContext>
+    export let musicContext: Writable<MusicContext>;
 </script>
 
-<div class="inline-flex ml-2 bg-gray-100 mt-2 h-10">
+<div class="inline-flex ml-2 mt-2 h-10">
     <div class="flex text-sm">
-        <div class="flex items-center hover:bg-gray-200 cursor-default pr-2">
-            <span class="ml-4 mr-2 text-sm">Time Signature</span>
+        <div class="pl-1 pr-4 flex items-center cursor-default">
+            <span class="text-sm pr-1 text-nowrap">Time Signature</span>
             <Select
                 items={TimeSignature.allNumerators().map((n) => {
                     return { value: n, name: n };
@@ -20,7 +24,8 @@
                     $musicContext.reInitialiizePatternStr();
                 }}
                 placeholder="N"
-                class="bg-gray-100 hover:bg-gray-300 py-0 w-20 border-none rounded-none text-center text-align-center hover:cursor-pointer"
+                size="sm"
+                defaultClass="border-none hover:bg-gray-50 w-20  text-center focus:ring-0 cursor-pointer"
             />
             <Select
                 items={TimeSignature.allDenominators().map((n) => {
@@ -28,17 +33,17 @@
                 })}
                 bind:value={$musicContext.timeSignature.denominator}
                 placeholder="D"
-                class="bg-gray-100 hover:bg-gray-300 py-0 w-16 border-l-1 border-r-0 border-t-0 border-b-0 rounded-none text-center"
                 size="sm"
+                defaultClass="border-none hover:bg-gray-50 select-none w-20 text-center focus:ring-0 cursor-pointer"
                 on:change={() => {
                     $musicContext.reInitialiizePatternStr();
                 }}
             />
         </div>
         <div
-            class="flex items-center border-2 border-gray-200 border-r-0 border-t-0 border-b-0 hover:bg-gray-200 cursor-default pr-2"
+            class="flex items-center border-2 border-gray-100 border-r-0 border-t-0 border-b-0 cursor-default px-4"
         >
-            <span class="text-sm ml-4 mr-2">Pattern</span>
+            <span class="text-sm pr-1 text-nowrap">Pattern</span>
             <Select
                 items={$musicContext.timeSignature
                     .complexityPatterns()
@@ -47,48 +52,48 @@
                     })}
                 bind:value={$musicContext.complexityPatternStr}
                 placeholder="complexity pattern"
-                class="bg-gray-100 p-0 w-40 border-none rounded-none text-center hover:bg-gray-300"
                 size="sm"
+                defaultClass="border-none hover:bg-gray-50 select-none w-40 text-center focus:ring-0 cursor-pointer"
             />
         </div>
         <div
-            class="flex bg-gray-100 items-center border-2 border-gray-200 border-r-0 border-t-0 border-b-0 hover:bg-gray-200 cursor-default pr-2"
+            class="flex items-center border-2 border-gray-100 border-r-0 border-t-0 border-b-0 cursor-default px-4"
         >
-            <span class="text-sm ml-4 mr-2">BPM</span>
+            <span class="text-sm pr-1 text-nowrap">BPM</span>
             <Input
                 bind:value={$musicContext.bpm}
                 type="number"
                 id="bpm"
                 placeholder="bpm"
-                required
-                class="bg-gray-100 hover:bg-gray-300 w-20 border-none rounded-none p-0 text-center text-sm"
+                size="sm"
+                class="border-none rounded-none bg-white hover:bg-gray-50 select-none w-20 text-center focus:ring-0 cursor-pointer"
             />
         </div>
         <div
-            class="flex items-center border-2 border-gray-200 border-r-0 border-t-0 border-b-0 hover:bg-gray-200 cursor-default pr-2"
+            class="flex items-center border-2 border-gray-100 border-r-0 border-t-0 border-b-0  cursor-default px-4"
         >
-            <span class="text-sm ml-4 mr-2">Division</span>
+            <span class="text-sm text-nowrap pr-2">Division</span>
             <Select
                 items={allDivisions().map((n) => {
                     return { value: n, name: n };
                 })}
                 bind:value={$musicContext.division}
                 placeholder="division"
-                class="bg-gray-100 hover:bg-gray-300 p-0 w-32 border-none rounded-none text-center text-sm"
+                defaultClass="border-none hover:bg-gray-50 select-none w-30 text-center focus:ring-0 cursor-pointer"
                 size="sm"
             />
         </div>
         <div
-            class="flex items-center border-2 border-gray-200 hover:bg-gray-200 border-r-0 border-t-0 border-b-0 pr-2"
+            class="flex items-center border-2 border-gray-100 border-r-0 border-t-0 border-b-0 px-4"
         >
-            <span class="text-sm ml-4 mr-2">Tuplet</span>
+            <span class="text-sm pr-1 text-nowrap">Tuplet</span>
             <Select
                 items={allTuplets().map((n) => {
                     return { value: n, name: n };
                 })}
                 bind:value={$musicContext.tuplet}
                 placeholder="tuplet"
-                class="bg-gray-100 hover:bg-gray-300 p-0 w-32 border-none rounded-none text-center text-sm"
+                defaultClass="border-none hover:bg-gray-50 select-none w-30 text-center focus:ring-0 cursor-pointer"
                 size="sm"
             />
         </div>
