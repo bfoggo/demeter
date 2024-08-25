9b036e815d82e83fe01ce8cc103a18d786010e27
bfoggo <b.j.foggo@gmail.com>
2024-08-25 11:44:53 -0500

    Made chord blocks independently editable

--- frontend/src/components/chordGrid.svelte
+++ frontend/src/components/chordGrid.svelte
@@ -12,8 +12,18 @@
         parsedChord: ChordGrammar;
     } = $props();
 
-    function parseChordInput(input: string) {
+    let selectedMeasure = $state(0);
+    let allChords: ChordGrammar[] = $derived.by(() => {
+        return Array.from({ length: musicSettings.complexityPattern.length * musicSettings.measures }, (_, i) => {
+            return { root: { noteSet: "sharps", name: "C" } } as ChordGrammar;
+        });
+    });
+
+    $inspect(allChords)
+
+    function parseChordInput(input: string, idx: number) {
         parsedChord = parseChord(input, "sharps");
+        allChords[idx] = parsedChord;
     }
 
 </script>
@@ -22,19 +32,27 @@
     class="w-2/3 grid grid-cols-8 auto-cols-max grid-flow-row-dense gap-1 mr-4"
 >
     {#each Array.from({ length: musicSettings.complexityPattern.length * musicSettings.measures }, (_, i) => i) as measure}
-        <div class="col-span-1 h-9 border-2 border-black">
+        <div class="col-span-1 h-9">
             <input
                 type="text"
                 class="w-full text-center h-full text-sm"
-                value={parsedChord.root.name}
+                
+                value={allChords[measure].root.name}
+                onclick={(e) => {
+                    const target = e.target as HTMLInputElement;
+                    if (target){
+                        selectedMeasure = measure;
+                        parseChordInput(target.value, measure);
+                    }
+                }
+            }
                 onchange={(e) => {
                     const target = e.target as HTMLInputElement;
                     if (target){
-                        parseChordInput(target.value);
+                        parseChordInput(target.value, measure);
                     }}
                 }
             >
-            <input>
         </div>
         
     {/each}
